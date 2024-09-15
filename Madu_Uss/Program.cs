7c696d4d5df61f31b451c3e7d3f129ca19f416a9
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-09-16 01:40:12 +0300

    Update Program.cs
    kõik 15 õppetundi läbitud

--- Madu_Uss/Program.cs
+++ Madu_Uss/Program.cs
@@ -1,88 +1,102 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 
-namespace Madu_Uss
+namespace Madu_Uus
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(80, 25);
 
-            Point p1 = new Point(1, 3, '*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            //p2.x = 4;
-            //p2.y = 5;
-            //p2.sym = '#';
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
-
-
-            //----------------------------------------------------
-            ////tund 6
-            //List<int> numList = new List<int>();
-            //numList.Add(0);
-            //numList.Add(1);
-            //numList.Add(2);
-
-            //int x = numList[0];
-            //int y = numList[1];
-            //int z = numList[2];
-
-            //foreach (int i in numList)
-            //{ 
-            //    Console.WriteLine(i);
-            //}
-
-            //numList.RemoveAt(0); 
-
-            //List<Point> pList = new List<Point>();
-            //pList.Add(p1);
-            //pList.Add(p2);
-            //---------------------------------------------------
-
-            //---------------------------------------------------
-            //tund 1-2
-            //int x1 = 1;
-            //int y1 = 3;
-            //char sym1 = '*';
-
-            //Draw( x1, y1, sym1 );
-
-            //int x2 = 4;
-            //int y2 = 5;
-            //char sym2 = '#';
-
-            //Draw( x2, y2, sym2 );
-            //------------------------------------------------------
-
-
-            Console.SetWindowSize(80, 27);
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
-            Snake snake = new Snake(p, 4, Directory.RIGHT);
-            snake.Drow();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-        }                                                                                                                                                                      
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw(food.x, food.y, food.sym);
+
+            while (true) 
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw(food.x, food.y, food.sym);
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+            }
+
+
+
+
+
+
+
+
+
+
+
+            //    VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            //    Draw(v1);
+
+            //    Point p = new Point(4, 5, '*');
+            //    Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            //    Draw (fSnake);
+            //    Snake snake = (Snake)fSnake;
+
+
+            //    HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
+
+            //    List<Figure> figures = new List<Figure>();
+            //    figures.Add(fSnake);
+            //    figures.Add(v1);
+            //    figures.Add(h1);
+
+            //    foreach(var f in figures)
+            //    {
+            //        f.Draw();
+            //    }
+            //}
+            //static void Draw(Figure figure)
+            //{
+            //    figure.Draw();
+            //}
+
+            //static void Main(string[] args)
+            //{
+            //    Console.SetWindowSize(80, 25);
+
+            //    HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            //    HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            //    VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            //    VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            //    upLine.Draw();
+            //    downLine.Draw();
+            //    leftLine.Draw();
+            //    rightLine.Draw();
+
+            //    Point p = new Point(4, 5, '*');
+            //    Snake snake = new Snake(p, 4, Direction.RIGHT);
+            //    snake.Draw();
+            //    snake.Move();
+        }
     }
 }
