b42671b8787502e7119af93d85923d09813e0014
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-09-16 15:35:31 +0300

    Update Program.cs

--- Madu_Uss/Program.cs
+++ Madu_Uss/Program.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Text;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Madu_Uus
 {
@@ -13,9 +8,12 @@ namespace Madu_Uus
     {
         static void Main(string[] args)
         {
+            int foodCounter = 0;
+            string gameOver = "Game Over";
+            string message = "Iidi, võtta jalad alla ja muru peal jaluta!";
             Console.SetWindowSize(80, 25);
 
-            Walls walls = new Walls(25, 80);
+            Walls walls = new Walls(26, 80);
             walls.Draw();
 
             Point p = new Point(4, 5, '*');
@@ -26,22 +24,61 @@ namespace Madu_Uus
             Point food = foodCreator.CreateFood();
             food.Draw(food.x, food.y, food.sym);
 
-            while (true)
+            bool gameOverFlag = false;
+            bool messageDisplayed = false; // Флаг для проверки, было ли сообщение показано
+
+            // Создаем и запускаем таймер
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            while (!gameOverFlag)
             {
                 if (walls.IsHit(snake) || snake.IsHitTail())
                 {
-                    break;
-                }
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw(food.x, food.y, food.sym);
+                    gameOverFlag = true;
+                    Console.Clear();
+                    int x = Console.WindowWidth;
+                    int y = Console.WindowHeight;
+                    Console.SetCursorPosition((x - gameOver.Length) / 2, y / 2);
+                    Console.WriteLine(gameOver);
                 }
                 else
                 {
-                    snake.Move();
+                    if (snake.Eat(food))
+                    {
+                        // Очистка старой еды
+                        Console.SetCursorPosition(food.x, food.y);
+                        Console.Write(' ');
+
+                        food = foodCreator.CreateFood();
+                        foodCounter += 1;
+                        food.Draw(food.x, food.y, food.sym);
+                    }
+                    else
+                    {
+                        snake.Move();
+                    }
                 }
+
+                // Обновляем счетчик еды и таймер
+                Console.SetCursorPosition(0, 0);
+                Console.Write($"Toit söödud: {foodCounter}");
+
+                // Показываем прошедшее время
+                TimeSpan elapsedTime = stopwatch.Elapsed;
+                Console.SetCursorPosition(Console.WindowWidth - 15, 0);
+                Console.Write($"Aeg: {elapsedTime.Minutes:D2}:{elapsedTime.Seconds:D2}");
+
+                // Проверяем, прошло ли 2 минуты и не было ли сообщение уже показано
+                if (elapsedTime.TotalMinutes >= 1 && !messageDisplayed)
+                {
+                    Console.SetCursorPosition((Console.WindowWidth - message.Length) / 2, Console.WindowHeight / 2);
+                    Console.WriteLine(message);
+                    messageDisplayed = true; // Устанавливаем флаг, что сообщение показано
+                }
+
                 Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
@@ -49,59 +86,10 @@ namespace Madu_Uus
                 }
             }
 
-
-
-
-
-
-
-
-
-
-
-            //    VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            //    Draw(v1);
-
-            //    Point p = new Point(4, 5, '*');
-            //    Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            //    Draw (fSnake);
-            //    Snake snake = (Snake)fSnake;
-
-
-            //    HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            //    List<Figure> figures = new List<Figure>();
-            //    figures.Add(fSnake);
-            //    figures.Add(v1);
-            //    figures.Add(h1);
-
-            //    foreach(var f in figures)
-            //    {
-            //        f.Draw();
-            //    }
-            //}
-            //static void Draw(Figure figure)
-            //{
-            //    figure.Draw();
-            //}
-
-            //static void Main(string[] args)
-            //{
-            //    Console.SetWindowSize(80, 25);
-
-            //    HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            //    HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            //    VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            //    VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            //    upLine.Draw();
-            //    downLine.Draw();
-            //    leftLine.Draw();
-            //    rightLine.Draw();
-
-            //    Point p = new Point(4, 5, '*');
-            //    Snake snake = new Snake(p, 4, Direction.RIGHT);
-            //    snake.Draw();
-            //    snake.Move();
+            // Ожидаем нажатие клавиши перед завершением программы
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.WriteLine("Väljumiseks vajutage suvalist klahvi...");
+            Console.ReadKey();
         }
     }
 }
